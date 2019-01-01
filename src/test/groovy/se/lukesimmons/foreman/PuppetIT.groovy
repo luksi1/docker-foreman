337de9439963a996ddebead2d97b0fc23dc943c0
Luke Simmons <luke.simmons@gmail.com>
2019-01-01 21:44:08 +0100

    working example of docker-maven-plugin

---
+++ src/test/groovy/se/lukesimmons/foreman/PuppetIT.groovy
@@ -0,0 +1,8 @@
+#!/usr/bin/env groovy
+
+import junit.framework.Test
+import junit.textui.TestRunner
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.Matchers.is
+import static org.hamcrest.Matchers.equalTo
+import static org.junit.matchers.JUnitMatchers.*
