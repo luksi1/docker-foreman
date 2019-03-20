1aabe957d492ac923540db2aacee60b3b4fac41a
luksi1 <luke.simmons@vgregion.se>
2019-03-20 22:16:59 +0100

    cleanup with hadolint + added travis

--- foo.groovy
+++ foo.groovy
@@ -1,33 +1,43 @@
+#!/usr/bin/env groovy
 
 @Grab(group='org.hamcrest',module='hamcrest-all',version='1.3')
-//@Grab(group='org.apache.commons',module='commons-text',version='1.3')
-//@Grab(group='org.codehaus.groovy.modules',module='groovyws',version='0.5.2')
+@Grab(group='org.apache.commons',module='commons-text',version='1.3')
+@Grab(group='org.codehaus.groovy.modules',module='groovyws',version='0.5.2')
 @Grab(group='org.codehaus.groovy.modules.http-builder',module='http-builder',version='0.7.1')
-//@Grab(group='org.codehaus.groovy',module='groovy-json',version='2.5.6')
+@Grab(group='org.codehaus.groovy',module='groovy-json',version='2.5.6')
+
+
+import groovyx.net.http.*
+import org.apache.http.auth.*
+import org.apache.http.impl.client.*
+import org.apache.http.client.params.*
+import org.apache.http.auth.params.*
+import groovy.xml.*
+import groovy.json.JsonSlurper
+
 
 
 def password
-def foremanPort = "329832986"
+def foremanPort = "32992"
 
-(["/usr/bin/docker","ps"].execute().text =~ /(\S+).*foreman:latest.*/).each { fullContainerId, containerId -> (["/usr/bin/docker","exec","-t",containerId,"foreman-rake","permissions:reset"].execute().text =~ /Reset to user: admin, password: (\S+)\s+/).each { full, match -> password = match }
-}
+(["/usr/bin/docker","ps"].execute().text =~ /(\S+).*foreman:latest.*/).each { fullContainerId, containerId -> (["/usr/bin/docker","exec","-t",containerId,"foreman-rake","permissions:reset"].execute().text =~ /Reset to user: admin, password: (\S+)\s+/).each { full, match -> password = match } }
 println(password)
 
-    String url = "https://localhost:" + foremanPort
-    Foreman f = Foreman.getInstance()
-    String user = 'admin'
-    String password = f.adminPassword
-    String base64UsernamePassword = user + ":" + password.bytes.encodeBase64().toString()
+String url = "https://localhost:" + foremanPort
+String user = 'admin'
+String usernamePassword = user + ":" + password
+String base64UsernamePassword = usernamePassword.bytes.encodeBase64().toString()
+println(base64UsernamePassword)
 
-    HTTPBuilder remote = new HTTPBuilder(url)
-    remote.ignoreSSLIssues()
-    remote.setHeaders([Authorization: "Basic ${base64UsernamePassword}"])
+HTTPBuilder remote = new HTTPBuilder(url)
+remote.ignoreSSLIssues()
+remote.setHeaders([Authorization: "Basic ${base64UsernamePassword}"])
 
-    remote.request(Method.GET) { req ->
-      uri.path = "/api/v2/dashboard"
-      response.success = { resp, json ->
-        assertEquals((int)resp.status, 200)
-        assertEquals((int)json.total_hosts, 0)
-      }
-    }
+remote.request(Method.GET) { req ->
+  uri.path = "/api/v2/dashboard"
+  response.success = { resp, json ->
+    println(resp.status)
+    println(json.total_hosts)
+  }
+}
 
--- src/test/groovy/se/lukesimmons/foreman/ForemanIT.groovy
+++ src/test/groovy/se/lukesimmons/foreman/ForemanIT.groovy
@@ -12,14 +12,24 @@ import org.apache.commons.text.StringEscapeUtils
 import groovy.json.JsonSlurper
 import static org.junit.matchers.JUnitMatchers.*
 
+/**
+Run foreman-rake against the image to reset and grab Foreman's admin password.
+We should be able to run commands from the Docker Engine API, but for simplicity, just grab it from the
+command line.
+*/
 public class Foreman {
 
   private static Foreman instance;
 
   public String adminPassword;
-    
+
   private Foreman(){
-    (["/usr/bin/docker","ps"].execute().text =~ /(\S+).*foreman:latest.*/).each { fullContainerId, containerId -> (["/usr/bin/docker","exec","-t",containerId,"foreman-rake","permissions:reset"].execute().text =~ /Reset to user: admin, password: (\S+)\s+/).each { full, match -> adminPassword = match }
+    (["/usr/bin/docker","ps"].execute().text =~ /(\S+).*foreman:latest.*/).each { 
+      fullContainerId, containerId -> 
+        (["/usr/bin/docker","exec","-t",containerId,"foreman-rake","permissions:reset"].execute().text =~ /Reset to user: admin, password: (\S+)\s+/).each { 
+          full, match -> 
+            adminPassword = match
+        }
     }
   }
     
@@ -41,7 +51,8 @@ class ForemanIT extends GroovyTestCase {
     Foreman f = Foreman.getInstance()
     String user = 'admin'
     String password = f.adminPassword
-    String base64UsernamePassword = user + ":" + password.bytes.encodeBase64().toString()
+    String usernamePassword = user + ":" + password
+    String base64UsernamePassword = usernamePassword.bytes.encodeBase64().toString()
 
     HTTPBuilder remote = new HTTPBuilder(url)
     remote.ignoreSSLIssues()
