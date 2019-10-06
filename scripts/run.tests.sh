f97910efcc3b974ce190fa00758bf2a6b3136b77
Luke Simmons <luke.simmons@gmail.com>
2019-10-07 00:16:26 +0200

    remove fully qualified path for travis binaries

--- scripts/run.tests.sh
+++ scripts/run.tests.sh
@@ -7,7 +7,7 @@ FOREMAN_CONTAINER_ID=$(/usr/bin/sudo /usr/bin/docker ps -qaf label=org.label-sch
 # grab password from foreman
 ADMIN_PASSWORD=$(/usr/bin/sudo /usr/bin/docker exec -t "$FOREMAN_CONTAINER_ID" foreman-rake permissions:reset | /bin/grep 'Reset to user:' | /usr/bin/awk '{print $NF}' | tr -d '\r')
 # swap out admin password with jq
-/usr/bin/jq ".auth.basic[0].value = \"${ADMIN_PASSWORD}\"" tests/foreman.json > tests/foreman.json.bak && mv tests/foreman.json.bak tests/foreman.json
+jq ".auth.basic[0].value = \"${ADMIN_PASSWORD}\"" tests/foreman.json > tests/foreman.json.bak && mv tests/foreman.json.bak tests/foreman.json
 # perform tests
-/usr/bin/newman run tests/foreman.json --insecure
+newman run tests/foreman.json --insecure
 
