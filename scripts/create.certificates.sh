b3e768208a876fcfef411c4c4a0ec242f331bd41
luksi1 <luke@vscode.us-central1-a.c.playground-250603.internal>
2019-10-06 03:30:47 +0000

    added smart-proxy cert + removed puppetdb tests

--- scripts/create.certificates.sh
+++ scripts/create.certificates.sh
@@ -2,13 +2,17 @@
 
 export FOREMAN_URL='foreman.dummy.test'
 export PUPPET_URL='puppet.dummy.test'
+export PUPPET_SMARTPROXY_URL='puppet-smarty-proxy.dummy.test'
 
 sudo -E docker run -h "$PUPPET_URL" -v /etc/puppetlabs/puppet/ssl:/etc/puppetlabs/puppet/ssl puppet/puppetserver:latest ca setup
 sudo -E docker network create -d bridge foreman
 # Start the instance
 sudo -E docker run --network foreman -d --name puppet -h puppet.dummy.test -v /etc/puppetlabs/puppet/ssl:/etc/puppetlabs/puppet/ssl puppet/puppetserver:latest
 sleep 15
-sudo -E docker exec -t puppet puppetserver ca generate --certname foreman.dummy.test
+# Generate a foreman certificate signed by puppet
+sudo -E docker exec -t puppet puppetserver ca generate --certname "$FOREMAN_URL"
+# Generate a smart proxy certificate signed by puppet
+sudo -E docker exec -t puppet puppetserver ca generate --certname "$PUPPET_SMARTPROXY_URL"
 # cleanup images
 sudo -E docker rm -f puppet
 sudo -E docker network rm foreman
